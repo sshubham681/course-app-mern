bb1a9e3ff97b588bde2f9c2813e704ac7481dc40
Shubham Verma <subham.varma@reachmobile.com>
2024-10-08 17:55:40 +0530

    added signup functionality and data is updating in the db

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const UserModel = require("../models/user-models");
 const router = express.Router();
 
 // user signup
-router.post("/signup", function (req, res) {
-  res.send({
-    msg: "Sign up",
-  });
+router.post("/signup", async function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  try {
+    const foundUser = await UserModel.findOne({ username });
+    console.log(foundUser?.username, "--f");
+    console.log(username, "--username");
+    if (foundUser && foundUser?.username === username) {
+      res.send({
+        message: "User already exist!",
+      });
+    } else {
+      await UserModel.create({
+        username,
+        password,
+      });
+      res.send({
+        message: "User got created!",
+      });
+    }
+  } catch (error) {
+    console.log(error, "error");
+    res.status(201).send({
+      message: "Failed to create user.",
+    });
+  }
 });
 // user login
 router.post("/login", function (req, res) {
