a9848d00e46413bb903a9f1b28cae693e510bb01
Shubham Verma <subham.varma@reachmobile.com>
2024-10-08 08:59:18 +0530

    added dotenv support for port

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+require('dotenv').config()
 const adminRoutes = require("./routes/admin.routes");
 const userRoutes = require("./routes/user.routes");
+
+const PORT = process.env.PORT || 8000
 const connectToDB = require("./db/db");
 
 const app = express();
@@ -9,6 +12,6 @@ connectToDB()
 app.use("/admin", adminRoutes);
 app.use("/user", userRoutes);
 
-app.listen(8000, () => {
-  console.log("server is running on 8000");
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}/`);
 });
