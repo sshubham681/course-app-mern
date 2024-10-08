29332f6c7ff500387bd37675e9000876ee9bf60a
Shubham Verma <subham.varma@reachmobile.com>
2024-10-08 17:55:11 +0530

    fixed timestamp and syntax of models

--- models/course-model.js
+++ models/course-model.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const Course = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String },
-  price: { type: Number, required: true },
-  isFree: { type: Boolean, default: false },
-});
+const CourseSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String },
+    price: { type: Number, required: true },
+    isFree: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
-const CourseModel = mongoose.model("courses", Course);
+const CourseModel = mongoose.model("courses", CourseSchema);
 
 module.exports = CourseModel;
--- models/user-models.js
+++ models/user-models.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-const User = new mongoose.Schema({
-  username: { type: String, unique: true, required: true },
-  password: { type: String, unique: true, required: true },
-  timestamp: true,
-});
+const User = new mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-const UserModel = mongoose.model(User, "user");
-
+const UserModel = mongoose.model("users", User);
 module.exports = UserModel;
